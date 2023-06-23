ec238cb89bb36282c46cbf5eefdd0843b3236b0d
hariz <harizsharizad@gmail.com>
2023-06-23 10:53:55 +0800

    counting related models

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -28,6 +28,7 @@ class RealtorListingController extends Controller
                     ->listings()
 //                    ->mostRecent()
                     ->filter($filters)
+                    ->withCount('images')
                     ->paginate(5)
                     ->withQueryString()
             ]
