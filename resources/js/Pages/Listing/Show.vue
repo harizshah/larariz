16a84fde0c6ae264d0cd958dd1e4a64c9889c595
hariz <harizsharizad@gmail.com>
2023-06-26 20:25:45 +0800

    notification page backend

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-        <Box class="md:col-span-7 flex items-center">
-            <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+        <Box v-if="listing.images.length" class="md:col-span-7 flex items-center">
+            <div class="grid grid-cols-2 gap-1">
                 <img
                     v-for="image in listing.images" :key="image.id"
                     :src="image.src"
                 />
             </div>
-            <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
         </Box>
+        <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
+
         <div class="md:col-span-5 flex flex-col gap-4">
             <Box>
                 <template #header>
@@ -89,6 +90,7 @@ import { usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
 import MakeOffer from "@/Pages/Listing/Show/Components/MakeOffer.vue";
 import OfferMade from './Show/Components/OfferMade.vue'
+import EmptyState from "@/Components/UI/EmptyState.vue";
 
 const offer = ref(props.listing.price)
 const interestRate = ref(2.5)
